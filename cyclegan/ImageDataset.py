5cefae190872dd8d5c49ca94239cc33b972ca2f3
Sheng Yang <shengyang.b@gmail.com>
2019-11-25 22:58:37 -0600

    Modify imageDataset to accept grey scale

--- cyclegan/ImageDataset.py
+++ cyclegan/ImageDataset.py
@@ -6,19 +6,54 @@ from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 
+
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
 class ImageDataset(Dataset):
     def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
         self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
 
-        #print(sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*')))
         self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
         self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
 
     def __getitem__(self, index):
+        image_A = Image.open(self.files_A[index % len(self.files_A)])
+
+        if self.unaligned:
+            image_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
+        else:
+            image_B = Image.open(self.files_B[index % len(self.files_B)])
+
+        # Convert grayscale images to rgb
+        if image_A.mode != "RGB":
+            image_A = to_rgb(image_A)
+        if image_B.mode != "RGB":
+            image_B = to_rgb(image_B)
+
+        item_A = self.transform(image_A)
+        item_B = self.transform(image_B)
+        return {"A": item_A, "B": item_B}
+
+        """
         item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
-        item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+
+        if self.unaligned:
+            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+        else:
+            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+
+
+        # Convert grayscale images to rgb
+        if item_A.mode != "RGB":
+            item_A = to_rgb(item_A)
+        if item_B.mode != "RGB":
+            item_B = to_rgb(item_B)
 
         return {'A': item_A, 'B': item_B}
-
+        """
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
